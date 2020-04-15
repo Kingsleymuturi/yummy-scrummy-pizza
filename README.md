bccde2c2e40f8a754cb95a48cf59416288042fe1
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-04-15 21:37:57 +0300

    add initial webpage

---
+++ README.md
@@ -0,0 +1,44 @@
+# Yummy Scrummy Pizza
+<table>
+<tr>
+<td>
+  
+#### This is a webpage that enhances customer service by offering pizza deliveries, Current version 2020
+</table>
+</tr>
+</td>
+
+#### By **Kingsleymuturi**
+  
+## Description
+Yummy Scrummy Website is a website that helps users get pizza at their doorsteps by offering quick deliveries countrywide and also has a section for users to give feedback on our service.
+## Access
+* click the live link to be directed directly to the webpage
+https://Kingsleymuturi.github.io/yummy-scrummy-pizza
+![yummy scrummy pizza website](pizza/images/yummy.png)
+* This section allows users to give us their feedback
+![feedback section](pizza/images/feedback.png)
+* This is where our esteemed cutomers place their orders
+![Order placing section](pizza/images/order.png)
+## Known Bugs
+Some texts might duplicate themselves due to button errors
+### Development
+Want to contribute? Great!
+
+To fix a bug or enhance an existing module, follow these steps:
+
+- Fork the repo
+- Create a new branch (`git checkout -b improve-feature`)
+- Make the appropriate changes in the files
+- Add changes to reflect the changes made
+- Commit your changes (`git commit -m 'Improve feature'`)
+- Push to the branch (`git push origin improve-feature`)
+- Create a Pull Request 
+
+## Technologies Used
+During the creation of this webpage I used HTML, CSS and also JavaScript came in handy along with jQuery
+## Support and contact details
+In case of anything find me here: kingsleymuturi9@gmail.com feel free to communicate any issue with the webpage
+
+### [LICENSE](https://github.com/Kingsleymuturi/yummy-srummy-pizza/blob/master/LICENSE)
+Copyright (c) 2020 [Kingsley Muturi ](https://github.com/Kingsleymuturi)
---
+++ js/scripts.js
@@ -0,0 +1,150 @@
+var price, crust_price, topping_price;
+let total = 0;
+function getPizza(size, crust, topping, number, total) {
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+	this.number = number;
+	this.total = total;
+}
+// proceed button
+$(document).ready(function () {
+  //$("button.proceed").click(function(){
+  //$("button.proceed").hide();
+  //$("#information").hide();
+  //$("div.choice").slideDown(1000);
+	//});
+	$("button.proceed").click(function(event){
+    let psize = $("#size option:selected").val();
+		let pcrust = $("#crust option:selected").val();
+		let pnumber = $("#number").val();
+    let ptopping = [];
+    $.each($("input[name='toppings']:checked"), function () {
+      ptopping.push($(this).val());
+    });
+    
+		
+		switch (psize) {case "0": price = 0; break;
+		case "small": price = 600; break;
+		case "medium": price = 850; break;
+    case "large": price = 1200; break;
+    default:console.log("error");}
+		
+	if (psize == "large"){
+		topping_value = ptopping.length *100;
+	} else if(psize == "medium"){
+		topping_value = ptopping.length *70;
+	} else if(psize == "small"){
+		topping_value = ptopping.length *50;
+	}
+
+	switch (pcrust) {case "0": crust_price = 0; break;
+	case "Chicago Style Pan": crust_price = 300; break;
+	case "Flat Bread": crust_price = 200; break;
+	case "Stuffed Dough": crust_price = 150; break;
+	case "Beer Battered": crust_price = 180; break;
+	case "Neopolitan": crust_price = 150; break;
+	case "St. Louis": crust_price = 250; break;
+	default: console.log("No price");}
+
+	if (psize == "0") {
+		$("button.proceed").show();
+		$("#information").show();
+		$("div.choice").hide();
+		document.getElementById("Invalid").innerText = "Please select size!"
+    document.getElementById("Invalid").style.color ="yellow"
+	} else if(pcrust == "0"){
+		$("button.proceed").show();
+		$("#information").show();
+		$("div.choice").hide();
+		document.getElementById("Invalid1").innerText = "Please select crust!"
+    document.getElementById("Invalid1").style.color ="yellow"
+	}  else if(ptopping.length == 0){
+		$("button.proceed").show();
+		$("#information").show();
+		$("div.choice").hide();
+		document.getElementById("Invalid3").innerText = "Please select at least one topping!"
+    document.getElementById("Invalid3").style.color ="yellow"
+	}
+	else if(pnumber <= 0 || pnumber > 100){
+		$("button.proceed").show();
+		$("#information").show();
+		$("div.choice").hide();
+		document.getElementById("Invalid2").innerText = "Please select quantity among 1-50!"
+    document.getElementById("Invalid2").style.color ="yellow"
+	}
+	 else {
+		$("button.proceed").hide();
+		$("#information").hide();
+		$("div.choice").slideDown(1000);
+	}
+
+	total = pnumber*(price + crust_price + topping_value);
+    let checkoutTotal = 0;
+		checkoutTotal = checkoutTotal + total;
+	
+		
+    $("#pizzasize").html($("#size option:selected").val());
+		$("#pizzacrust").html($("#crust option:selected").val());
+		$("#pizzanumber").html($("#number").val());
+    $("#pizzatopping").html(ptopping.join(", "));
+		$("#totals").html(total);
+  
+	
+		$("button#checkout").click(function (event) {event.preventDefault();
+      $("button#checkout").hide();
+      $("button.deliver").slideDown(1000);
+      $("#addedprice").slideDown(1000);
+      $("#pizzatotal").append("Your bill is sh. " + checkoutTotal);
+		});
+		
+		$("button.deliver").click(function (event) {event.preventDefault();
+      $(".pizzatable").hide();
+      $(".choice h2").hide();
+      $(".delivery").slideDown(1000);
+      $("#addedprice").hide();
+      $("button.deliver").hide();
+      $("#pizzatotal").hide();
+      let delamount = checkoutTotal + 300;
+      console.log("You will pay sh. " + delamount + " on delivery");
+      $("#totalbill").append("Your bill plus delivery fee is: " + delamount);
+		});
+		
+		$("button#final-order").click(function (event) {
+      event.preventDefault();
+
+      $("#pizzatotal").hide();
+      $(".delivery").hide();
+      $("button#final-order").hide();
+      let delamount = checkoutTotal + 300;
+      console.log("Final Bill is: " + delamount);
+      let person = $("input#name").val();
+      let phone = $("input#phone").val();
+      let location = $("input#location").val();
+
+      if ($("input#name").val() && $("input#phone").val() && $("input#location").val() != "") {
+
+        $("#finallmessage").append(person + ", " + phone +", We have recieved your order and it will be delivered to you at " + location + ". Prepare sh. " + delamount);
+        $("#totalbill").hide();
+        $("#finallmessage").slideDown(1200);
+      }
+      else {
+        alert("Please fill in the details for delivery!");
+        $(".delivery").show();
+        $("button#final-order").show();
+      }
+    });
+    event.preventDefault();
+	});
+});
+
+
+
+function sendMsg() {
+	var name = $("#NAME").val();
+	var email = $("#EMAIL").val();
+	var message = $("#MESSAGE").val();
+	if (name !== "" && email !== "" && message !== ""){
+		alert("Hey " + name + "! Thank you for your message! We will review it and give you feedback. Check your email for our feedback soon.")
+  }
+};
